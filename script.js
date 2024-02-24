7b7388f98aca64cfc3eacfce2a6650cd257d7bb5
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2024-02-24 09:19:42 +0300

    Initial commit (Add objects). This commit adds objects to my application and hold its initial stage mark up.

---
+++ script.js
@@ -0,0 +1,48 @@
+const myLibrary = [
+    {
+        title: "Rich dad poor dad",
+        author: "Robert Kiyosaki",
+        pages: 1000,
+        readOrNot: "read",
+        info: function() {
+            return `${this.title} by ${this.author}, ${this.pages}, ${this.readOrNot}`;
+        },
+    },
+    {
+        title: "Rich dad poor dad",
+        author: "Robert Kiyosaki",
+        pages: 1500,
+        readOrNot: "not read",
+        info: function() {
+            return `${this.title} by ${this.author}, ${this.pages}, ${this.readOrNot}`;
+        },
+    }
+];
+
+function Book(title, author, pages, readOrNot) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readOrNot = readOrNot;
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.readOrNot}`;
+    };
+}
+
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+
+function addBookToLibrary() {
+    let newBook = prompt("Please enter a book");
+    let newBookArray = newBook.split(", ")
+    let newBookObject = new Book(newBookArray[0], newBookArray[1], newBookArray[2], newBookArray[3]);
+    myLibrary.push(newBookObject);
+}
+
+function displayBooks() {
+    for (const book of myLibrary) {
+        console.log(book.info());
+    }
+}
+
+displayBooks();
